7fc6121ca6c8cf4f03521286d2316d29e96e0e5b
EmilianoCelleri <emiliano.celleri@gmail.com>
2025-04-24 21:41:34 -0300

    correciones

--- client.py
+++ client.py
@@ -10,11 +10,8 @@ print(f"Recibido del servidor: {message.decode()}")
 client_socket.send(b"Hola, servidor! Soy el cliente.")
 
 while message.strip().lower() != 'exito':
-
     #Enviar mensajes
     message = input("Enviar mensaje: ")
     client_socket.send(message.encode())
-    #Recibir confirmacion de envio
-    message = client_socket.recv(1024)
-    print(f"Recibido del servidor: {message.decode()}")
+
 
